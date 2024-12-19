fb6309caf69b4e1efe4022dedcbf154b8df0ae4b
mequam <blue9ja@gmail.com>
2024-12-19 10:03:37 -0500

    fuzzy testing finished :D

--- DAKMatrix/test.py
+++ DAKMatrix/test.py
@@ -3,42 +3,60 @@ from matrix import Matrix
 import random
 from typing import Callable
 
-class TestMatrixMethods(unittest.TestCase):
+#array of functions that the module uses to create values for testing matricies
+number_generators = [
+          lambda : random.random()*100,
+          lambda : random.randint(0, 100),
+          lambda : complex(random.random()*100,random.random()*100)
+          ]
 
-    def test_diagonalization(self):
-        
-        inv_diag_eigvec = Matrix.Diagonal([1,2,3])
+"""
+repeats the decorated function test amount times accross each of the different number generators
+for testing functions over several different classes and many random values
+"""
+def fuzzy_test(test_amount,**kwargs):
+    def decorator(f):
+
+        verbose = kwargs["verbose"] if "verbose" in kwargs else False
+
+        def ret_val(*args,**kwargs):
+            
+            if verbose:
+                print()
+                print(f"fuzzy testing  {f.__name__} {test_amount*len(number_generators)} ({test_amount}x{len(number_generators)}) times...")
+
+            for i in range(test_amount):
+                for gen in number_generators:
+                    creator = lambda x : (gen() for i in range(x))
+                    f(*args,creator,**kwargs)
+            
+            if verbose:
+                print("done!")
+
+        return ret_val
+
+    return decorator
+
+class TestMatrixMethods(unittest.TestCase):
+    @fuzzy_test(100,verbose=True)
+    def test_diagonalization(self,gen):
+        a,b,c = gen(3)
+
+        inv_diag_eigvec = Matrix.Diagonal([a,b,c])
 
         value = Matrix.Identity(3)
         for x in reversed(inv_diag_eigvec.diagonalize()):
             value = x*value
 
-        self.assertEqual(Matrix([[1,0,0],
-                                 [0,2,0],
-                                 [0,0,3]]), value)
+        self.assertEqual(Matrix([[a,0,0],
+                                 [0,b,0],
+                                 [0,0,c]]), value)
 
-    def test_add(self):
-        #verify that we add for SEVERAL different values
-        #accross several different types
-
-        for i in range(300):
-            self.verify_random_add(lambda : random.random()*100)
-
-            self.verify_random_add(lambda : random.randint(0, 100))
-
-            self.verify_random_add(lambda : complex(
-                                            random.random()*100,
-                                            random.random()*100)
-                                  )
-
-    
-    """
-    generates two random matrix from the given function
-    and verifies that they add properly
-    """
-    def verify_random_add(self,generator : Callable[[],any])->None:
-        a,b,c,d = [generator() for i in range(4)]
-        e,f,g,h = [generator() for i in range(4)]
+    @fuzzy_test(100,verbose=True)
+    def test_add(self,gen):
+        a,b,c,d = gen(4)
+        e,f,g,h = gen(4)
+        
 
         m1 = Matrix([[a,b],[c,d]])
         m2 = Matrix([[e,f],[g,h]])
@@ -46,7 +64,6 @@ class TestMatrixMethods(unittest.TestCase):
         self.assertEqual(m1+m2, m2+m1)
         self.assertEqual(m1+m2, Matrix([[a+e,b+f],[c+g,d+h]]))
         self.assertEqual(m2+m1, Matrix([[a+e,b+f],[c+g,d+h]]))
-
 
     def test_multiply(self):
         m1 = Matrix([[1,1],[2,1]])
